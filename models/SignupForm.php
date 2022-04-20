7a06af57a513caf7c2a96fc5cccd1c9c6150de53
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-20 23:46:47 +0300

    rules

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -3,7 +3,20 @@
 namespace app\models;
 
 use yii\base\Model;
+use yii\debug\models\router\RouterRules;
+use yii\rbac\Rule;
 
 class SignupForm extends Model
 {
+    public $username;
+    public $password;
+    public $password_repeat;
+
+    public function rules()
+    {
+        return [
+
+            [['username', 'password', 'password_repeat'], 'required'],
+        ];
+    }
 }

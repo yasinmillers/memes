6a9cb02f6a1ae1ab080ae3f406f5a04f25985c1f
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-21 12:44:38 +0300

    ss

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -4,6 +4,7 @@ namespace app\models;
 
 use yii\base\Model;
 use yii\debug\models\router\RouterRules;
+use yii\helpers\VarDumper;
 use yii\rbac\Rule;
 
 class SignupForm extends Model
@@ -28,5 +29,11 @@ class SignupForm extends Model
         $user->password = \yii::$app->security->generatePasswordHash($this->password);
         $user->access_token = \yii::$app->security->generateRandomString();
         $user->auth_key = \yii::$app->security->generateRandomString();
+
+        if ($user->save()) {
+            return true;
+        }
+        \yii::error(message: "user not saved " . VarDumper::dumpAsString($user->errors));
+        return false;
     }
 }

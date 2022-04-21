35c5a4fe7cb305f875b7a1112022f7ac67ba0488
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-21 13:13:11 +0300

    signup

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,6 +64,11 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionSignup()
+    {
+        return $this->render('index');
+    }
+
     /**
      * Login action.
      *

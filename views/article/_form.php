d891871dbc49e0e339a0a2505250f07e89a5746a
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-25 11:34:37 +0300

    form

--- views/article/_form.php
+++ views/article/_form.php
@@ -18,11 +18,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
